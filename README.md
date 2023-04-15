31d55fb7713d0bafca6db63f581ed7a208f8cd1e
Sam Schweigel <s.schweigel@gmail.com>
2023-04-14 23:42:53 -0700

    Update readme

--- README.md
+++ README.md
@@ -1,10 +1,44 @@
 # CPEN 400Q final project: Experimental Quantum GANs
 
+![Batch GAN circuit](https://user-images.githubusercontent.com/33556084/232192493-dd3f1fc5-7bc6-494a-beae-6ade8cb9bd27.png)
+
+This repository contains software for reproducing some of the result from
+[Experimental Quantum Generative Adversarial Networks for Image Generation
+](https://arxiv.org/abs/2010.06201) by He-Liang Huang et. al.
+
+```python
+
+```
+
+Links:
+- [Companion report](report/report.pdf)
+- [Presentation](report/presentation.pdf)
+- [Demo notebook](demo/talk-demo.ipynb)
+  This also makes a good quickstart for learning the library API.
+
 ## Quickstart
+
+If you use [Poetry](https://python-poetry.org/) already, getting started is
+easy:
 
 ```
 $ poetry install
 $ poetry run jupyterlab  # Run the demo notebooks
+$ poetry run python scripts/figures.py  # Reproduce the report figures
+
+```
+
+If you have a lot of CPU cores and time, you can also generate the bar plots:
+```
+$ poetry run python scripts/plots.py  # Reproduce the report bar plots
+```
+
+If you python environment looks like ![XKCD
+1987](https://imgs.xkcd.com/comics/python_environment.png), a container image
+can be built:
+
+```
+$ docker build -t quantumgan . && docker run -it --rm -p 8888:8888 quantumgan poetry run jupyter lab --allow-root --ip 0.0.0.0 --no-browser
 ```
 
 ## Software
@@ -20,6 +54,10 @@ this package.  A brief overview:
 - `classical`: Classical GANs for the performance comparisons
 
 Public APIs have docstrings, while private methods start with `_` and do not.
+
+The scripts used for generating the data in the report are in `scripts/`.  These
+are not part of the public API, but might be interesting regardless, as examples
+of how the library can be used.
 
 ### Development notes
 
--- scripts/figures.py
+++ scripts/figures.py
@@ -2,7 +2,10 @@ import matplotlib
 import matplotlib.pyplot as plt
 import jax.random as jr
 import pennylane as qml
+import optax
 
+from quantumgan.train import train_gan
+import quantumgan.datasets as datasets
 from quantumgan.datasets import generate_grayscale_bar
 from quantumgan.batch import BatchGAN
 
@@ -21,7 +24,7 @@ fig, ax = plt.subplots(2, 4)
 for row in range(2):
     for col in range(4):
         ax[row, col].matshow(data[row, col])
-fig.savefig("plots/bar_data.pdf")
+fig.savefig("report/plots/bar_data.pdf")
 
 gen_params, dis_params = BatchGAN.init_params(key, 4, 3, 1, 4, 1)
 gan = BatchGAN(4, 1, gen_params, dis_params)
@@ -29,5 +32,56 @@ gan = BatchGAN(4, 1, gen_params, dis_params)
 qml.draw_mpl(gan._qnode_train_fake)(
     gen_params, dis_params, gan.random_latent(key, 1)
 )
-plt.savefig("plots/batch_gan_circuit.pdf")
+plt.savefig("report/plots/batch_gan_circuit.pdf")
 plt.close()
+
+key = jr.PRNGKey(1)
+key, params_key = jr.split(key)
+features_dim = 4
+batch_size = 1
+
+gen_params, dis_params = BatchGAN.init_params(
+    params_key,
+    features_dim,
+    gen_layers=3,
+    gen_ancillary=1,
+    dis_layers=4,
+    dis_ancillary=1,
+)
+
+
+gan = BatchGAN(
+    features_dim,
+    1,
+    gen_params,
+    dis_params,
+)
+
+key, train_key = jr.split(key)
+
+gen_optimizer = optax.sgd(0.05)
+dis_optimizer = optax.sgd(0.001)
+
+key, data_key = jr.split(key)
+train_data = datasets.generate_grayscale_bar(data_key, 5000).reshape(
+    -1, batch_size, 4
+)
+
+train_result = train_gan(
+    train_key,
+    gan,
+    gen_optimizer,
+    dis_optimizer,
+    train_data,
+    show_progress=True,
+)
+
+latent = gan.random_latent(jr.PRNGKey(0), 8)
+# Final checkpoint
+data = train_result.checkpoints[-1][1].generate(latent).reshape(2, 4, 2, 2)
+
+fig, ax = plt.subplots(2, 4)
+for row in range(2):
+    for col in range(4):
+        ax[row, col].matshow(data[row, col])
+fig.savefig("report/plots/generated_bars.pdf")
